d8d6eac859f6f080ff5f6b1856ffbd28b0a05767
arkan dzaky <arkanoor09@gmail.com>
2025-08-01 16:24:16 +0700

    add aliases.zsh as omz custom

---
+++ omz/.oh-my-zsh/custom/aliases.zsh
@@ -0,0 +1,31 @@
+# App Launcher
+alias vivaldi="/Applications/Vivaldi.app/Contents/MacOS/Vivaldi"
+
+# Git Aliases
+alias gs="git status"
+alias ga="git add"
+alias gc="git commit -m"
+alias gp="git push"
+alias gd="git diff"
+alias gl="git log --oneline"
+
+alias gaa="git add --all"
+alias gss="git status -s"
+alias gcl="git clone"
+alias glg="git log --oneline --graph"
+alias gpm="git push origin main"
+alias gch="git checkout"
+
+alias gmot="git remote -v"
+alias gset="git reset --soft HEAD~1"
+alias gsto="git restore --staged"
+
+alias gsetm="git reset --mixed HEAD~1"
+
+# Nix Aliases
+alias nfu="nix flake update"
+alias snfu="sudo nix flake update"
+alias nsp="nix search nixpkgs"
+alias nsh="nix-shell -p"
+alias das="darwin-rebuild switch"
+alias sdas="sudo darwin-rebuild switch"
